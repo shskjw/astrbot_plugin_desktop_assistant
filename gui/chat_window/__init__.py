b67549a96fa354ab35661d20c65b72936d83aa65
木有知 <152591788+muyouzhi6@users.noreply.github.com>
2025-12-22 16:01:25 +0800

    Delete gui directory

--- gui/chat_window/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-对话窗口组件模块
-"""
-
-from .chat_window import ChatWindow
-
-__all__ = ["ChatWindow"]
--- gui/floating_ball/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-悬浮球组件模块
-"""
-
-from .ball_window import FloatingBallWindow
-
-__all__ = ["FloatingBallWindow"]
